530301cf5f73fc030100bc349ae43d539886216f
Marcos Yuri <marcosyuri.fsz@gmail.com>
2020-06-13 19:52:34 -0300

    first commit

---
+++ server/src/routes.ts
@@ -0,0 +1,19 @@
+import express, { response } from 'express'
+import knex from './database/connection'
+import PointsController from './controllers/pointsController'
+import ItemsController from './controllers/itemsController'
+
+
+const routes = express.Router()
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
+
+routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id',pointsController.show)
+routes.get('/items',itemsController.index)
+
+
+
+
+export default routes
