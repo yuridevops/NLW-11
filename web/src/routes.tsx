bf4fa1070df3c8ffc603a8fb557be16a525c8bbc
Marcos Yuri <marcosyuri.fsz@gmail.com>
2020-06-13 20:00:36 -0300

    first commit

---
+++ web/src/routes.tsx
@@ -0,0 +1,15 @@
+import { Route, BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import Home from './pages/Home'
+import CreatePoint from './pages/CreatePoint'
+
+const Routes = () => {
+    return(
+        <BrowserRouter>
+            <Route component={Home} exact path='/'/>
+            <Route component={CreatePoint} exact path='/create-point'/>
+        </BrowserRouter>
+    )
+}
+
+export default Routes
